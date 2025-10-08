8520d951d5360097f905c5c58c2b4b2fe3518c8b
LILA84140 <lilabouabba06@gmail.com>
2025-10-08 12:42:19 +0200

    correction code final

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -54,7 +54,7 @@ class ListeSimpleTest {
  listeATester.ajout(3);
  System.out.println(listeATester);
 
- assertEquals("[1, 2, 3]", listeATester.toString());
+ assertEquals("ListeSimple(Noeud(3), Noeud(2), Noeud(1))", listeATester.toString());
  }
 
  @Test
